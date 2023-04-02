bfc74c00e92fcd64e101053c3bdbbed1a660b265
Carlo Caballero <carlopcaballero@gmail.com>
2023-04-02 17:01:00 -0400

    Setup thought controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,38 @@
-const { User, Thought } = require("../models");
+const { Thought, Thought } = require("../models");
+
+module.exports = { 
+
+newThought(req, res) {
+    Thought.create(req.body)
+        .then(({ _id}) => {
+            return User.findOneAndUpdate(
+                { _id: req.body.userId },
+                { $push: { thoughts: _id } },
+                { new: true }
+            );
+}).then((thought) => res.json(thought));
+},
+
+editThought(req, res) {
+    Thought.findOneAndUpdate(
+        {_id: req.params.thoughtID},
+        {$set: req.body},
+    ).then((thought) => res.json(thought));
+},    
+
+deleteThought(req, res) {
+    Thought.findOneAndDelete({_id: req.params.thoughtID})
+    .then((thought) => res.json(thought));
+},
+
+getThoughts(req, res) {
+    Thought.find({})
+        .then((thought) => res.json(thought));
+},
+
+getOneThought(req, res) {
+    Thought.findOne({_id: req.params.thoughtID})
+    .then(( thought) => res.json(thought));
+},
+
+};
